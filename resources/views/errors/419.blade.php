49b7c449b1f0a2b3c01fc3ae354d590804f323ca
ShockFromBL <12955300+ShockFromBL@users.noreply.github.com>
2024-01-03 17:42:11 +0000

    Add 419 (Page Expired) error page

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,10 @@
+@extends('templates.app')
+
+@section('title', 'Page Expired')
+
+@section('description', 'Your session timed out, please go back and refresh the page.')
+
+@section('content')
+    <h2>Page Expired</h2>
+    <p>Your session timed out, please go back and refresh the page.</p>
+@endsection
