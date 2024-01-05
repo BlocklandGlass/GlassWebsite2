751d9f5c82827e0ec7f4bfd828d2549c946f1ee2
ShockFromBL <12955300+ShockFromBL@users.noreply.github.com>
2024-01-05 01:43:13 +0000

    Add noindex and rename

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,6 +4,10 @@
 
 @section('description', $user->name)
 
+@section('head')
+    <meta name="robots" content="noindex">
+@endsection
+
 @section('content')
     <h2>{{ $user->name }}</h2>
     <p>This is {{ $user->name }}.</p>
