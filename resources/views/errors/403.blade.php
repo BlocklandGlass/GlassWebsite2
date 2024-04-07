c7002286b1534daba955d59f3137f6d3eb2384ec
ShockFromBL <12955300+ShockFromBL@users.noreply.github.com>
2024-04-07 18:16:57 +0100

    Add better 403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('templates.app')
+
+@section('title', 'Forbidden')
+
+@section('description', 'You don\'t have permission to access this.')
+
+@section('content')
+    <h2>Forbidden</h2>
+    <p>You don't have permission to access this.</p>
+@endsection
